46e9c28e216421811f6e336168bf83b94353391a
Roman A. Shvetsov <tcp6000@gmail.com>
2024-10-02 16:31:06 +0300

    Implemented test of "close" method of Connection Interface.

---
+++ tests/Unit/Connection/ConnectionCloseTest.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dorpmaster\Nats\Tests\Unit\Connection;
+
+use Amp\Socket\Socket;
+use Amp\Socket\SocketConnector;
+use Dorpmaster\Nats\Connection\Connection;
+use Dorpmaster\Nats\Domain\Connection\ConnectionConfigurationInterface;
+use Dorpmaster\Nats\Domain\Connection\ConnectionException;
+use PHPUnit\Framework\TestCase;
+use Psr\Log\LoggerInterface;
+
+final class ConnectionCloseTest extends TestCase
+{
+    public function testClose(): void
+    {
+        $socket = self::createMock(Socket::class);
+
+        $connector = self::createMock(SocketConnector::class);
+        $connector->method('connect')
+            ->with('test.nats.local:4222')
+            ->willReturn($socket);
+
+        $configuration = self::createMock(ConnectionConfigurationInterface::class);
+        $configuration->method('getHost')
+            ->willReturn('test.nats.local');
+        $configuration->method('getPort')
+            ->willReturn(4222);
+        $configuration->method('getQueueBufferSize')
+            ->willReturn(1000);
+
+        $logger = self::createMock(LoggerInterface::class);
+
+        $connection = new Connection(
+            $connector,
+            $configuration,
+            $logger,
+        );
+
+        self::assertTrue($connection->isClosed());
+        $connection->open();
+        self::assertFalse($connection->isClosed());
+        $connection->close();
+        self::assertTrue($connection->isClosed());
+    }
+}
